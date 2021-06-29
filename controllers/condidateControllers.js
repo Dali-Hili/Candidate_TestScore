4a6ef68f81177e0ce063e4ccd17d3464aa72de6c
Dali-Hili <you@example.com>
2021-06-29 22:25:22 +0200

    setting score works

--- controllers/condidateControllers.js
+++ controllers/condidateControllers.js
@@ -9,3 +9,31 @@ module.exports.createCondidate = async (req, res) => {
         res.send(err);
     }
 }
+
+//find all condidate 
+module.exports.findAllCondidate = async (req, res) => {
+    try {
+        const condidates = await condidateModule.find()
+        res.send(condidates)
+    } catch (err) {
+        res.send(err);
+    }
+}
+
+//Insert score for condidate  
+module.exports.insertScore = async (req, res) => {
+    const score = {
+        test_score: {
+            first_round : req.body.first_round , 
+            second_round : req.body.second_round, 
+            third_round: req.body.third_round}
+    };
+    try {
+        let condidates = await condidateModule.findOne({ email: req.body.email})
+        let insertScore = await condidates.updateOne({$set: score});
+        res.send(insertScore);
+        console.log(score);
+    } catch (err) {
+        res.send(err);
+    }
+}
--- routes/condidate.js
+++ routes/condidate.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const condidateController = require('../controllers/condidateControllers')
+
 //Create condidate
 router.post("/create", condidateController.createCondidate)
+
+//find all condidate 
+router.get("/", condidateController.findAllCondidate)
+
+//insert condidate score
+router.put("/insertScore", condidateController.insertScore)
+
 module.exports = router
